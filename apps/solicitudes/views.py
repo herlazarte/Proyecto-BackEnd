4f28d87cfb4edc5145c05058f5b0caa6e43725de
herlazarte <agusmixherlazarte@gmail.com>
2024-11-22 16:41:25 -0300

    resolvi problema que ingresaba cualquier profesional y veia las solicitudes que acepto o rechazo otro

--- apps/solicitudes/views.py
+++ apps/solicitudes/views.py
@@ -4,25 +4,42 @@ from django.views.generic import TemplateView, ListView
 from apps.solicitudes.models import Solicitud
 from django.db.models import Exists, OuterRef, Subquery
 from django.db.models import Case, When, Value, CharField
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import Q
+
+
 
 
 from apps.turnos.models import Turno
+from apps.usuarios.models import Profesional
 
-# Create your views here.
-
-class SolicitudesView(ListView):
+class SolicitudesView(LoginRequiredMixin, ListView):
     model = Solicitud
     template_name = 'listado_solicitudes.html'
     context_object_name = 'solicitudes'
 
     def get_queryset(self):
-        # Agregar el campo 'turno_asignado' y 'estado_turno' a cada solicitud
-        return Solicitud.objects.annotate(
-            turno_asignado=Exists(Turno.objects.filter(solicitud=OuterRef('pk'))),
+        # Obtener el usuario autenticado
+        usuario = self.request.user
+        
+        # Obtener la instancia del Profesional correspondiente al usuario logueado
+        try:
+            profesional = Profesional.objects.get(usuario=usuario)
+        except Profesional.DoesNotExist:
+            # Si el usuario no es profesional, simplemente mostramos las solicitudes generales
+            return Solicitud.objects.all()
+        
+        # Filtrar las solicitudes que pertenecen al profesional logueado o no tienen turno asignado
+        solicitudes = Solicitud.objects.filter(
+            Q(turno__profesional=profesional) | Q(turno=None)
+        ).annotate(
+            turno_asignado=Exists(Turno.objects.filter(solicitud=OuterRef('pk'), profesional=profesional)),
             estado_turno=Subquery(
-                Turno.objects.filter(solicitud=OuterRef('pk')).values('estado')[:1]
+                Turno.objects.filter(solicitud=OuterRef('pk'), profesional=profesional).values('estado')[:1]
             )
         )
+        
+        return solicitudes
 
 class SolicitudesClienteView(TemplateView):
     template_name = "dashboard_cliente.html"
